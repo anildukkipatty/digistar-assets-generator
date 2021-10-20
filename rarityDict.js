433846482bc736111d2738eb591e543d1820996e
Anil Dukkipatty <anildukkipatty@gmail.com>
2021-10-20 21:26:32 +0530

    rarity dict WIP

---
+++ rarityDict.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+console.log(process.argv[2]);
+
+let existingRarity = {};
+let data = [];
+let baseURI = process.argv[2];
+
+try {
+  const temp = fs.readFileSync(`${baseURI}/rarity.csv`)
+    .toString("ascii")
+    .split('\n')
+    .map(l => l.trim())
+    .map(l => l.split(',').map(e => e.trim()))
+    .forEach(item => {
+      if (!existingRarity[item[0]])
+        existingRarity[item[1]] = parseInt(item[2])
+    });
+} catch (error) {
+  console.log('Couldnt load from existing rarity file');
+}
+
+try {
+  data = JSON.parse(fs.readFileSync(`${baseURI}/meta-data.json`).toString('ascii'))
+    .map(c => ({...c, rarity: (existingRarity[c.fileName] !== undefined ? existingRarity[c.fileName] : 0)}))
+} catch (error) {
+  console.error(`Couldnt load/find the meta-data file`);
+}
+
+const resStringified = data.map(c => `${c.folder},${c.fileName},${c.rarity}`).join('\n');
+fs.writeFileSync(`${baseURI}/rarity.csv`, resStringified);
