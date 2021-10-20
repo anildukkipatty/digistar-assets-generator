6e1175b9da601dc925f3191123191d2302a2fdbc
Anil Dukkipatty <anildukkipatty@gmail.com>
2021-10-20 22:23:11 +0530

    lowercased

--- rarityDict.js
+++ rarityDict.js
@@ -14,7 +14,7 @@ try {
     .map(l => l.split(',').map(e => e.trim()))
     .forEach(item => {
       if (!existingRarity[item[0]])
-        existingRarity[item[1]] = parseInt(item[2])
+        existingRarity[item[1].toLowerCase()] = parseInt(item[2])
     });
 } catch (error) {
   console.log('Couldnt load from existing rarity file');
@@ -22,7 +22,7 @@ try {
 
 try {
   data = JSON.parse(fs.readFileSync(`${baseURI}/meta-data.json`).toString('ascii'))
-    .map(c => ({...c, rarity: (existingRarity[c.fileName] !== undefined ? existingRarity[c.fileName] : 0)}))
+    .map(c => ({...c, rarity: (existingRarity[c.fileName.toLowerCase()] !== undefined ? existingRarity[c.fileName.toLowerCase()] : 0)}))
 } catch (error) {
   console.error(`Couldnt load/find the meta-data file`);
 }
